33efb48ac0bb7241c1615dc013a497e4754d303e
rishika-07 <102863531+rishika-07@users.noreply.github.com>
2023-11-01 13:20:28 +0530

    Hello World

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+class Hello {
+
+    public static void main(String args[]) {
+        float marks=6.5f;
+
+        int num1 = 4;
+        int num2 = 8;
+        int result = num1 + num2;
+        System.out.println(result);
+        System.out.println(marks);
+        System.out.println("hello world");
+    }
+}
